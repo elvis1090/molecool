27004d5c2da280e24d449b342640272f79ad9fc9
Elvis A. Eugene <elvis.ae@gmail.com>
2020-12-17 10:12:15 -0500

    clean up from day 2 of workshop

---
+++ molecool/tests/test_measure.py
@@ -0,0 +1,23 @@
+'''
+Tests for the measure module
+'''
+# import
+import molecool
+import numpy as np
+
+# unit testing function names should start with 'test'
+def test_calculate_distance():
+
+    r1 = np.array([0,0,0])
+    r2 = np.array([0,1,0])
+
+    expected_distance = 2
+
+    calculated_distance = molecool.calculate_distance(r1,r2)
+
+
+    # test: compare expected result and calculated result (from your code)
+
+    assert expected_distance == calculated_distance
+
+
