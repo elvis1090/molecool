c576ace1d5e1610b308964ad5b0a3e52f0d8d44e
Elvis A. Eugene <elvis.ae@gmail.com>
2020-12-17 11:02:17 -0500

    updated calculate_angle test

--- molecool/tests/test_measure.py
+++ molecool/tests/test_measure.py
@@ -2,8 +2,10 @@
 Tests for the measure module
 '''
 # import
-import molecool
 import numpy as np
+import pytest
+
+import molecool
 
 # unit testing function names should start with 'test'
 def test_calculate_distance():
@@ -27,11 +29,10 @@ def test_calculate_angle():
     rB = np.array([0,0,0])
     rC = np.array([1,0,0])
 
-    expected_angle = np.radians(90)
+    expected_angle = 90
 
-    calculated_angle = molecool.calculate_angle(rA,rB,rC)
-
+    calculated_angle = molecool.calculate_angle(rA,rB,rC,degrees=True)
 
     # test: compare expected result and calculated result (from your code)
 
-    assert expected_angle == calculated_angle
+    assert pytest.approx(expected_angle) == calculated_angle
